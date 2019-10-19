1efdb384e16d695777dce9bdc640146db7aaf4ff
Matheus Lemos <matheus@ajudici.com.br>
2019-10-19 14:20:39 -0300

    Added: Banco de dados
    Adicionada conexão com o mongodb e juncão dos outputs.

--- venv/src/satelliteEndpoints.py
+++ venv/src/satelliteEndpoints.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 import requests
-# import logging
+import database
 
 class SatelliteEndpoints(Resource):
 
@@ -21,13 +21,11 @@ class SatelliteEndpoints(Resource):
 		satelliteFields['name'] = data['info']['satname']
 
 		tle = data['tle']
-		satelliteFields['tle'] = self.parseTle(tle)
+		satelliteFields['launch_date'] = self.parseTle(tle)
 		return satelliteFields
 
 	def parsePosFields(self, data):
 		satelliteFields = {}
-		satelliteFields['id'] = data['info']['satid']
-		satelliteFields['name'] = data['info']['satname']
 		pos = data['positions'][0]
 		satelliteFields['pos'] = self.parsePos(pos)
 		return satelliteFields
@@ -51,7 +49,6 @@ class SatelliteEndpoints(Resource):
 
 		tleDict = {}
 		line1 = tle[0:70]
-		line2 = tle[71:]
 
 		year = line1[9:11]
 		if (int(year) > 20):
@@ -59,13 +56,7 @@ class SatelliteEndpoints(Resource):
 		else:
 			year = "20" + year
 
-		tleDict['year'] = year
-		tleDict['inclination'] = line2[8:16]
-		tleDict['ascending_node'] = line2[17:25]
-		tleDict['eccentricity'] = line2[26:33]
-		tleDict['perigee'] = line2[34:42]
-		tleDict['revolutions_per_day'] = line2[52:63]
-		return tleDict
+		return year
 
 # POSITION
 
@@ -75,9 +66,18 @@ class SatelliteEndpoints(Resource):
 	def get(self):
 		""" Endpoint que faz a requisicão dos dados dos satélites
 		"""
-		# tleFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/tle/39129&apiKey=PWRZDT-4JMAK7-FDEPZM-47V1")
+		tleFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/tle/39129&apiKey=PWRZDT-4JMAK7-FDEPZM-47V1")
 		positionFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=PWRZDT-4JMAK7-FDEPZM-47V1")
 		parsedPosFields = self.parsePosFields(positionFields)
+		parseTleFields = self.parseTleFields(tleFields)
 
-		return parsedPosFields
-		# return {'hello': 'world'}
+		finalDict = {}
+		finalDict.update(parseTleFields)
+		finalDict.update(parsedPosFields)
+		result = database.insertData(finalDict)
+		print(finalDict)
+		if result:
+			ret = True
+		else:
+			ret = False
+		return {'ok': ret}
