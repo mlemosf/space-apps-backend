6d2f43057d582f992e4afe7934d0ec692f3a742d
Matheus Lemos <matheus@ajudici.com.br>
2019-10-20 04:04:08 -0300

    Added: get dos dados de satélite
    Adicionada funcão get para buscar os dados dos satélites para o frontend, com suas dadas categorias.

--- venv/src/database.py
+++ venv/src/database.py
@@ -10,6 +10,14 @@ def insertData(data):
 		print('One post: {0}'.format(result.inserted_id))
 		ret = True
 	except Exception:
-		print("Couldn't find data on database")
+		print("Couldn't store data on database")
 		ret = False
 	return ret
+
+def getData():
+	try:
+		satellite = db.satellite
+		result = satellite.find({}, {'_id': False})
+	except Exception:
+		print("Couldn't find data on database")
+	return result
--- venv/src/satelliteEndpoints.py
+++ venv/src/satelliteEndpoints.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource
+from flask import request
+from bson.json_util import dumps
 import requests
 import database
 
@@ -10,19 +12,22 @@ class SatelliteEndpoints(Resource):
 		r = requests.get(url=url)
 		return r.json()
 
-	def parseTleFields(self, data):
+	def parseTleFields(self, data, category):
 		""" Realiza o parse dos campos retornados pela api
 		Arguments:
 			fields {dict} -- Campos retornados pela API
 		"""
 
 		satelliteFields = {}
-		satelliteFields['id'] = data['info']['satid']
+		satelliteFields['satid'] = str(data['info']['satid'])
 		satelliteFields['name'] = data['info']['satname']
-
+		satelliteFields['category'] = category
 		tle = data['tle']
 		satelliteFields['launch_date'] = self.parseTle(tle)
 		return satelliteFields
+
+
+# POSITION
 
 	def parsePosFields(self, data):
 		satelliteFields = {}
@@ -58,26 +63,36 @@ class SatelliteEndpoints(Resource):
 
 		return year
 
-# POSITION
 
-
-	# def getPositionFields()
-
-	def get(self):
+	def post(self):
 		""" Endpoint que faz a requisicão dos dados dos satélites
 		"""
-		tleFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/tle/39129&apiKey=PWRZDT-4JMAK7-FDEPZM-47V1")
+		satid = request.args.get('id')
+		key = request.args.get('key')
+		category = request.args.get('category')
+
+		tleFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/tle/{}&apiKey={}".format(satid, key))
 		positionFields = self.requestData("https://www.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/0/2/&apiKey=PWRZDT-4JMAK7-FDEPZM-47V1")
 		parsedPosFields = self.parsePosFields(positionFields)
-		parseTleFields = self.parseTleFields(tleFields)
+		parseTleFields = self.parseTleFields(tleFields, category)
 
 		finalDict = {}
 		finalDict.update(parseTleFields)
 		finalDict.update(parsedPosFields)
 		result = database.insertData(finalDict)
-		print(finalDict)
 		if result:
 			ret = True
 		else:
 			ret = False
 		return {'ok': ret}
+
+
+	def get(self):
+		""" Endpoint que busca o satélite por id
+		"""
+
+		obj = []
+		data = database.getData()
+		for d in data:
+			obj.append(d)
+		return obj
